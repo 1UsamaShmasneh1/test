e1fff155fd957d8b198eacaf5b396bb528055c1e
usama shmasneh <us.05.07.91@gmail.com>
2022-03-25 22:20:00 +0300

    add test page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TestPageComponent } from './test-page/test-page.component';
+import { NgForm } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    NgForm
   ],
   providers: [],
   bootstrap: [AppComponent]
