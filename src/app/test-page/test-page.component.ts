4c98631bcd40bab454d950d2ce93f41e6d958fd4
usama shmasneh <us.05.07.91@gmail.com>
2022-03-26 22:26:42 +0300

    add questions

--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+export interface Question{
+  question: string,
+  option1: string,
+  option2: string,
+  option3: string,
+  answer: number
+}
+
 @Component({
   selector: 'app-test-page',
   templateUrl: './test-page.component.html',
   styleUrls: ['./test-page.component.css']
 })
+
+
 export class TestPageComponent implements OnInit {
 
   constructor() { }
 
-  questions = [
+  questions:Question[] = [
     {
       question: 'https://iq-global-test.com/upload/media/default/0011/01/1_0_0.png',
       option1: 'https://iq-global-test.com/upload/media/default/0011/01/1_2_0.png',
       option2: 'https://iq-global-test.com/upload/media/default/0011/01/1_1_0.png',
       option3: 'https://iq-global-test.com/upload/media/default/0011/01/1_3_0.png',
-      answer: 1
+      answer: 1,
     },
     {
       question: 'https://iq-global-test.com/upload/media/default/0011/01/2_0_0.png',
@@ -35,7 +45,7 @@ export class TestPageComponent implements OnInit {
   ]
 
   handleSubmit(testForm:NgForm){
-
+    console.log(testForm.value);
   }
 
   ngOnInit(): void {
