6dca286db9af78eef4d584a2ea33dbd72e42ff6b
usama shmasneh <us.05.07.91@gmail.com>
2022-03-26 19:36:41 +0300

    add questions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { TestPageComponent } from './test-page/test-page.component';
-import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +16,7 @@ import { NgForm } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    NgForm
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/test-page/test-page.component.ts
+++ src/app/test-page/test-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-test-page',
@@ -8,6 +9,34 @@ import { Component, OnInit } from '@angular/core';
 export class TestPageComponent implements OnInit {
 
   constructor() { }
+
+  questions = [
+    {
+      question: 'https://iq-global-test.com/upload/media/default/0011/01/1_0_0.png',
+      option1: 'https://iq-global-test.com/upload/media/default/0011/01/1_2_0.png',
+      option2: 'https://iq-global-test.com/upload/media/default/0011/01/1_1_0.png',
+      option3: 'https://iq-global-test.com/upload/media/default/0011/01/1_3_0.png',
+      answer: 1
+    },
+    {
+      question: 'https://iq-global-test.com/upload/media/default/0011/01/2_0_0.png',
+      option1: 'https://iq-global-test.com/upload/media/default/0011/01/2_1_0.png',
+      option2: 'https://iq-global-test.com/upload/media/default/0011/01/2_5_0.png',
+      option3: 'https://iq-global-test.com/upload/media/default/0011/01/2_4_0.png',
+      answer: 2
+    },
+    {
+      question: 'https://iq-global-test.com/upload/media/default/0011/01/3_0_0.png',
+      option1: 'https://iq-global-test.com/upload/media/default/0011/01/3_1_0.png',
+      option2: 'https://iq-global-test.com/upload/media/default/0011/01/3_3_0.png',
+      option3: 'https://iq-global-test.com/upload/media/default/0011/01/3_2_0.png',
+      answer: 3
+    }
+  ]
+
+  handleSubmit(testForm:NgForm){
+
+  }
 
   ngOnInit(): void {
   }
